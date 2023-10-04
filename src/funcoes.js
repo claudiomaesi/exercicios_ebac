44287251cce26f6d03aa9a0f0b5e601410d74d07
Claudio Maesi <claudiomaesi@gmail.com>
2023-10-04 18:46:38 -0300

    commit inicial

---
+++ src/funcoes.js
@@ -0,0 +1,9 @@
+"use strict";
+// FUNÇÃO MUÇTIPLICAÇÃO
+const multiplicacao = (fator1, fator2) => (fator1 * fator2);
+const resultadoMultiplica = multiplicacao(10, 10);
+console.log(resultadoMultiplica);
+// FUNÇÃO SAUDAÇÃO
+const saudacao = (nome) => `Olá, ${nome}!`;
+const resultadoSaudacao = saudacao('Cláudio');
+console.log(resultadoSaudacao);
---
+++ src/funcoes.ts
@@ -0,0 +1,9 @@
+// FUNÇÃO MUÇTIPLICAÇÃO
+const multiplicacao = (fator1:number, fator2:number): number => (fator1 * fator2);
+const resultadoMultiplica = multiplicacao(10, 10);
+console.log(resultadoMultiplica);
+
+// FUNÇÃO SAUDAÇÃO
+const saudacao = (nome: string): string => `Olá, ${nome}!`;
+const resultadoSaudacao = saudacao('Cláudio');
+console.log(resultadoSaudacao);
